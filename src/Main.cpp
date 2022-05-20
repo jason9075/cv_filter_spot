54c21d14716e3403d5926cb64cc9d4d2d6f03cf6
jason9075 <jason9075@users.noreply.github.com>
2022-05-20 11:31:48 +0800

    now I can use opencv lib in main.cpp but lsp server said not found..

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,12 +1,13 @@
 // Your First C++ Program
-
+#include <opencv2/opencv.hpp>
 #include <iostream>
 #include "Math.h"
 
-int main() {
-    std::cout << "Hello World2!\n";
+int main(int argc, char** argv ) {
     int result = Multiply(3,2);
     std::cout << result; 
+    cv::Mat image;
+    image = cv::imread( argv[1], 1 );
     return 0;
 }
 
--- src/Math.h
+++
@@ -1,2 +0,0 @@
-
-int Multiply(int a, int b);
